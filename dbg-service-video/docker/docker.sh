fa7f888cbcf28111065274bde77e3f84951f7f74
DbgDebug <maxiaoha@live.com>
2021-08-08 02:10:11 +0800

    Delete docker.sh

--- dbg-service-video/docker/docker.sh
+++
@@ -1,9 +0,0 @@
-docker run -d -p 8600:8600 --name dbg-service-video \
---env PROFILES=dev \
---env DB_HOST=cdb-72nu8aw0.bj.tencentcdb.com \
---env DB_PORT=10037 \
---env DB_USER=device \
---env DB_PWD=dbg-mysql2019 \
---env JVM_OPTS='-server -Xmx256m -Xms128m' \
---env IS_DEBUG=true \
-dbg-service-video:v1.0
