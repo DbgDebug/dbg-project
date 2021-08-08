cec56bd12d050e81a83f7de70165c1db5db577cb
DbgDebug <maxiaoha@live.com>
2021-08-08 18:09:23 +0800

    docker配置-pay

--- dbg-service-pay/docker/run.sh
+++ dbg-service-pay/docker/run.sh
@@ -3,6 +3,6 @@ if [ -z "$PROFILES" ]; then
   PROFILES="dev"
 fi
 
-echo java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dfile.encoding=UTF-8 -Dspring.profiles.active=$PROFILES $JVM_OPTS /opt/dbg-service-pay-0.0.1-SNAPSHOT.jar
-java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dfile.encoding=UTF-8 -Dspring.profiles.active=$PROFILES $JVM_OPTS /opt/dbg-service-pay-0.0.1-SNAPSHOT.jar
+echo java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dfile.encoding=UTF-8 -Dspring.profiles.active=$PROFILES $JVM_OPTS /opt/dbg-service-pay-1.0.0-SNAPSHOT.jar
+java -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -Dfile.encoding=UTF-8 -Dspring.profiles.active=$PROFILES $JVM_OPTS /opt/dbg-service-pay-1.0.0-SNAPSHOT.jar
 
